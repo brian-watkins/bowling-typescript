a73f035e0e6b5155ab2010dca03f1a1eefc63a33
Brian Watkins <bwatkins@vmware.com>
2021-04-07 09:07:59 -0400

    Got started

--- specs/BowlingScoreCalculator.spec.ts
+++ specs/BowlingScoreCalculator.spec.ts
@@ -15,6 +15,54 @@ import { BowlingScoreCalculator } from '../src/BowlingScoreCalculator'
 // Assume: We only need to calculate the score at the end, once the game is over
 
 
-describe("bowling score calculator", () => {
+// 0. No pins --> 0
+// 1. No strikes or spares
+// 2. There's a spare
+// 2. There's a strike
+// 2. Perfect game --> 300
 
+describe("bowling score calculator", () => {
+  let subject: BowlingScoreCalculator
+
+  beforeEach(() => {
+    subject = new BowlingScoreCalculator();
+  })
+
+  context("when all gutter balls are thrown", () => {
+    it("returns a score of zero", () => {
+      rollFrames(0, 0, 10)
+
+      expect(subject.score()).to.equal(0)
+    })
+  })
+
+  context("when no strikes or spares are thrown", () => {
+    it("returns the sum of all the throws", () => {
+      rollFrames(3, 2, 10)
+
+      expect(subject.score()).to.equal(50)
+    })
+  })
+
+  context("when there's a spare", () => {
+    it("counts the next roll after the spare twice", () => {
+      subject.roll(8)
+      subject.roll(2)
+      subject.roll(7)
+      subject.roll(3)
+      rollFrames(3, 2, 7)
+      subject.roll(1)
+      subject.roll(1)
+
+      expect(subject.score()).to.equal(10 + 7 + 10 + 3 + (7 * 5) + 2)
+    })
+  })
+
+  const rollFrames = (firstRoll: number, secondRoll: number, numberOfFrames: number) => {
+    for (let i = 0; i < numberOfFrames; i++) {
+      subject.roll(firstRoll)
+      subject.roll(secondRoll)
+    }
+  }
 })
+
--- src/BowlingScoreCalculator.ts
+++ src/BowlingScoreCalculator.ts
@@ -1,4 +1,30 @@
 export class BowlingScoreCalculator {
+  private rolls: Array<number> = []
+
+  roll(rollScore: number) {
+    this.rolls.push(rollScore)
+  }
   
+  score(): number {
+    let totalScore = 0
+    for (let i = 0; i < this.rolls.length; i++) {
+      totalScore = totalScore + this.rolls[i]
+      if (this.isEndOfFrame(i)) {
+        if (this.isSpare(i)) {
+          totalScore = totalScore + this.rolls[i+1]  
+        }
+      }
+    }
+
+    return totalScore;
+  }
   
+  private isSpare(i: number) {
+    return this.rolls[i] + this.rolls[i - 1] === 10
+  }
+
+  private isEndOfFrame(rollIndex: number) {
+    return rollIndex % 2 === 1
+  }
+
 }
